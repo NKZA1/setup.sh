8e818e5e4ab1f1b0aff4fd88594d517fd928c234
NKZA1 <nklatuido@gmail.com>
2024-07-04 22:22:42 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,25 @@
+#!/bin/bash
+
 # Thiết lập đường dẫn home cho Termux
 home='/data/data/com.termux/files/'
 
-printf '\n\033[1;32m Tiến Hành Cài Dữ Liệu\033[0m\n\n'
+printf '\n\033[1;32mTiến Hành Cài Dữ Liệu\033[0m\n\n'
 # Hiển thị thông báo tiến hành cài dữ liệu
 
 termux-setup-storage
-apt update
-apt upgrade
-pkg update
-pkg upgrade
-pkg install python
+apt update && apt upgrade -y
+pkg update && pkg upgrade -y
+pkg install python -y
 
-# Nâng cấp các gói đã cài đặt lên phiên bản mới nhất
-
-printf '\n\033[1;32m Tiến Hành Cài Đặt Các Công Cụ Cần Thiết\033[0m\n\n'
-pkg install python
+# Cài đặt các thư viện Python cần thiết
+printf '\n\033[1;32mTiến Hành Cài Đặt Các Công Cụ Cần Thiết\033[0m\n\n'
 pip install requests
 pip install operator
-curl -s https://raw.githubusercontent.com/NKZA1/NKZTOL/main/NKZ.py -o $home/usr/bin/nkzz
-# Cài đặt thư viện requests cho Python
-chmod 777 $home/usr/bin/nkzz
-# Cấp quyền thực thi cho script nkzz
 
-printf '\n\033[1;32m Gõ \033[1;33mnkzz \033[1;32mĐể Vào Tool \n\n'
+# Tải script nkzz từ GitHub và cài đặt
+printf '\n\033[1;32mCài Đặt Script NKZZ\033[0m\n\n'
+curl -s https://raw.githubusercontent.com/NKZA1/NKZTOL/main/NKZ.py -o $home/usr/bin/nkzz
+chmod 777 $home/usr/bin/nkzz
+
+printf '\n\033[1;32mGõ \033[1;33mnkzz \033[1;32mĐể Vào Tool\n\n'
 # Hiển thị thông báo hướng dẫn cách chạy tool
