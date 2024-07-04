b1752b947d1627ac007b5e7e83416d9b413bf20e
NKZA1 <nklatuido@gmail.com>
2024-07-04 23:37:59 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,12 @@
-home='/data/data/com.termux/files'
-
-printf '\n\033[1;32mTiến Hành Cài Dữ Liệu\033[0m\n\n'
-
+home='/data/data/com.termux/files/'
+printf '\n\033[1;32m Tiến Hành Cài Dữ Liệu\033[0m\n\n'
 termux-setup-storage
-apt update && apt upgrade -y
-pkg update && pkg upgrade -y
-pkg install python -y
-
-printf '\n\033[1;32mTiến Hành Cài Đặt Các Công Cụ Cần Thiết\033[0m\n\n'
+apt update
+apt upgrade
+printf '\n\033[1;32m Tiến Hành Cài Python\033[0m\n\n'
+pkg install python
 pip install requests
-pip install operator
+curl -s https://raw.githubusercontent.com/NKZA1/NKZ/main/NKZ.py -o $home/usr/bin/NKZ
+chmod 777 $home/usr/bin/NKZ
 
-printf '\n\033[1;32mCài Đặt Script NKZZ\033[0m\n\n'
-curl -s https://raw.githubusercontent.com/NKZA1/NKZ/main/NKZ.py -o $home/usr/bin/nkzz
-chmod 777 $home/usr/bin/nkzz
-
-printf '\n\033[1;32mGõ \033[1;33mnkzz \033[1;32mĐể Vào Tool\n\n'
+printf '\n\033[1;32m Gõ \033[1;33mNKZ \033[1;32mĐể Vào Tool \n\n'
