902f4e5321ccd83b0236e0622b5363991ec3ef8c
NKZA1 <nklatuido@gmail.com>
2024-07-04 23:46:35 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ apt upgrade
 printf '\n\033[1;32m Tiến Hành Cài Python\033[0m\n\n'
 pkg install python
 pip install requests
-curl -s https://raw.githubusercontent.com/nkza1/nkz/main/nkz.py -o $home/usr/bin/nkz
+curl -s https://raw.githubusercontent.com/NKZA1/NKZ/main/NKZ.py -o $home/usr/bin/nkz
 chmod 777 $home/usr/bin/nkz
 
 printf '\n\033[1;32m Gõ \033[1;33mnkz \033[1;32mĐể Vào Tool \n\n'
