01bf55ae054ee9e47d43bf7e3a77060ee38f8e36
NKZA1 <nklatuido@gmail.com>
2024-07-04 23:34:25 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-HOME='/data/data/com.termux/files/home'
+home='/data/data/com.termux/files/home'
 
 printf '\n\033[1;32mTiến Hành Cài Dữ Liệu\033[0m\n\n'
 
@@ -12,7 +12,7 @@ pip install requests
 pip install operator
 
 printf '\n\033[1;32mCài Đặt Script NKZZ\033[0m\n\n'
-curl -s https://raw.githubusercontent.com/NKZA1/NKZ/main/NKZ.py -o $HOME/usr/bin/nkzz
-chmod 777 $HOME/usr/bin/nkzz
+curl -s https://raw.githubusercontent.com/NKZA1/NKZ/main/NKZ.py -o $home/usr/bin/nkzz
+chmod 777 $home/usr/bin/nkzz
 
 printf '\n\033[1;32mGõ \033[1;33mnkzz \033[1;32mĐể Vào Tool\n\n'
