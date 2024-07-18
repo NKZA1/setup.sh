e63d6f4176ce4f2d7178a159768fddae4e45e9b2
NKZA1 <nklatuido@gmail.com>
2024-07-18 09:46:49 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
+#!/bin/bash
+
 home='/data/data/com.termux/files/'
+
 printf '\n\033[1;32m Tiến Hành Cài Dữ Liệu\033[0m\n\n'
 termux-setup-storage
 apt update
-apt upgrade
+apt upgrade -y
+
 printf '\n\033[1;32m Tiến Hành Cài Python\033[0m\n\n'
-pkg install python
+pkg install python -y
 pip install requests
+
 curl -s https://raw.githubusercontent.com/NKZA1/NKZ/main/NKZ.py -o $home/usr/bin/nkz
 chmod 777 $home/usr/bin/nkz
 
