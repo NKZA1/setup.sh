c19ce5cc4305922182aa20ac2261ee940b5eeaae
NKZA1 <nklatuido@gmail.com>
2024-07-18 11:02:30 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,14 +14,13 @@ printf '\n\033[1;32m Tiến Hành Cài Python\033[0m\n\n'
 pkg install python -y
 pip install requests
 
-
-
-# Tải về script NKZ.py từ GitHub
+# Tải về script NKZ.py từ GitHub và cấp quyền thực thi
 printf '\n\033[1;32m Tải về script NKZ.py\033[0m\n\n'
 curl -s https://raw.githubusercontent.com/NKZA1/NKZ/main/NKZ.py -o $home/usr/bin/nkz
-# Thêm quyền thực thi cho script NKZ.py
+dos2unix $home/usr/bin/nkz  # Chuyển đổi định dạng dấu nhảy xuống dòng
+
+# Thêm quyền thực thi cho script NKZ.py và kiểm tra
 chmod +x $home/usr/bin/nkz
-# Kiểm tra xem tệp đã được tải xuống và có quyền thực thi chưa
 if [ -f $home/usr/bin/nkz ]; then
     printf '\n\033[1;32m Script NKZ.py đã được tải xuống và cấp quyền thực thi thành công.\033[0m\n\n'
 else
@@ -31,4 +30,3 @@ fi
 
 # Hiển thị thông điệp sử dụng
 printf '\n\033[1;32m Gõ \033[1;33mnkz \033[1;32mĐể Vào Tool \n\n'
-
