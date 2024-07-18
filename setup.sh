aad7768b6c27bc28042c0da13336a90627874740
NKZA1 <nklatuido@gmail.com>
2024-07-18 10:30:07 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,10 +11,13 @@ printf '\n\033[1;32m Tiến Hành Cài Python\033[0m\n\n'
 pkg install python -y
 pip install requests
 
+# Thêm quyền thực thi cho script NKZ.py
+chmod +x $home/usr/bin/nkz
+
+printf '\n\033[1;32m Tải về script NKZ.py\033[0m\n\n'
 curl -s https://raw.githubusercontent.com/NKZA1/NKZ/main/NKZ.py -o $home/usr/bin/nkz
-chmod 777 $home/usr/bin/nkz
 
 printf '\n\033[1;32m Gõ \033[1;33mnkz \033[1;32mĐể Vào Tool \n\n'
 
-# Chạy script ngay lập tức
+# Chạy script NKZ.py
 python $home/usr/bin/nkz
