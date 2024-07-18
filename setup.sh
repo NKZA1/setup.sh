a205c42ea0d989ad24ab8fa6bd15d0d37bfedfb8
NKZA1 <nklatuido@gmail.com>
2024-07-18 10:57:02 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,13 +14,13 @@ printf '\n\033[1;32m Tiến Hành Cài Python\033[0m\n\n'
 pkg install python -y
 pip install requests
 
-# Thêm quyền thực thi cho script NKZ.py
-chmod +x $home/usr/bin/nkz
+
 
 # Tải về script NKZ.py từ GitHub
 printf '\n\033[1;32m Tải về script NKZ.py\033[0m\n\n'
 curl -s https://raw.githubusercontent.com/NKZA1/NKZ/main/NKZ.py -o $home/usr/bin/nkz
-
+# Thêm quyền thực thi cho script NKZ.py
+chmod +x $home/usr/bin/nkz
 # Kiểm tra xem tệp đã được tải xuống và có quyền thực thi chưa
 if [ -f $home/usr/bin/nkz ]; then
     printf '\n\033[1;32m Script NKZ.py đã được tải xuống và cấp quyền thực thi thành công.\033[0m\n\n'
