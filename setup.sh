dd839be1943d05a45f057d6c744fd3daa570f220
NKZA1 <nklatuido@gmail.com>
2024-11-17 18:41:42 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ printf '\n\033[1;32m Tiến Hành Cài Python Packages\033[0m\n\n'
 pip install requests
 pip install cloudscraper
 pip install colorama
+pip install regex
 printf '\n\033[1;32m Tải về script NKZ.py\033[0m\n\n'
 curl -s https://raw.githubusercontent.com/NKZA1/NKTOLpulic/refs/heads/main/NKpilic.py -o $home/usr/bin/nkz
 dos2unix $home/usr/bin/nkz
